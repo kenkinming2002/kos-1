c01e216712db27f7ef9fee3107fc044066f99bea
KenKwok <kenkinming2002@gmail.com>
2022-09-10 17:43:10 +0800

    Implement debug_write() with framebuffer and serial

---
+++ src/boot/debug.h
@@ -0,0 +1,7 @@
+#ifndef BOOT_DEBUG
+#define BOOT_DEBUG
+
+void debug_init();
+void debug_write(const char *str);
+
+#endif // BOOT_DEBUG
--- src/boot/main.c
+++ src/boot/main.c
@@ -1,14 +1,7 @@
-char *fb = (char *)0xb8000;
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-{
-  fb[i] = c;
-  fb[i + 1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
-}
-
-#define FB_GREEN     2
-#define FB_DARK_GREY 8
+#include "debug.h"
 
 void kmain()
 {
-  fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+  debug_init();
+  debug_write("ABCDEFG");
 }

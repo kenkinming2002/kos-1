8a0d8c40adbb054ad52880eef78839216a6c7c05
KenKwok <kenkinming2002@gmail.com>
2022-09-10 18:18:23 +0800

    Fix header guard naming

--- src/core/assert.h
+++ src/core/assert.h
@@ -1,7 +1,7 @@
-#ifndef TYPES_ASSERT_H
-#define TYPES_ASSERT_H
+#ifndef CORE_ASSERT_H
+#define CORE_ASSERT_H
 
 void kassert(const char *file, int line);
 #define KASSERT(expr) do { if(!(expr)) kassert(__FILE__, __LINE__); } while(0)
 
-#endif // TYPES_ASSERT_H
+#endif // CORE_ASSERT_H

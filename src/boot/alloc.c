69e221078459885c00636712a73d74129a27aaf3
KenKwok <kenkinming2002@gmail.com>
2022-09-12 23:26:00 +0800

    Fix alloc_heap() and alloc_memory()

--- src/boot/alloc.c
+++ src/boot/alloc.c
@@ -9,14 +9,18 @@ void *alloc_heap(size_t size, size_t alignment)
 {
   static uintptr_t addr = (uintptr_t)heap_begin;
   addr = (addr + alignment - 1) / alignment * alignment;
+
+  void *ptr = (void *)addr;
   addr += size;
-  return (void *)addr;
+  return ptr;
 }
 
 void *alloc_memory(size_t size, size_t alignment)
 {
   static uintptr_t addr = (uintptr_t)memory_begin;
   addr = (addr + alignment - 1) / alignment * alignment;
+
+  void *ptr = (void *)addr;
   addr += size;
-  return (void *)addr;
+  return ptr;
 }

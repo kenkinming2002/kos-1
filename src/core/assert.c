ce54dfed1e5e8c3c50780f219fbd4bebb307f984
KenKwok <kenkinming2002@gmail.com>
2022-09-13 01:06:17 +0800

    Implement diagnostic for assertion failure

--- src/core/assert.c
+++ src/core/assert.c
@@ -1,6 +1,9 @@
 #include "assert.h"
 
+#include "print.h"
+
 void kassert(const char *file, int line)
 {
+  debug_printf("assertion failure file: %s, line: %d\n", file, line);
   for(;;) asm volatile("hlt");
 }

da89c77ae02d6324488427140ddb22e4dc6abe0b
KenKwok <kenkinming2002@gmail.com>
2022-09-13 14:58:28 +0800

    Map [0-2GB] to [0-2GB] and [2Gb-4GB] and enable paging in entry

--- src/boot/loader.s
+++ src/boot/loader.s
@@ -1,12 +1,13 @@
 .global entry
 .extern bmain
 
+/* Multiboot 2 header */
+.section .multiboot2
+
 .equ MULTIBOOT2_MAGIC,         0xE85250D6
 .equ MULTIBOOT2_ARCHITECTURE,  0x0
 .equ MULTIBOOT2_HEADER_LENGTH, multiboot2_header_end - multiboot2_header_begin
 .equ MULTIBOOT2_CHECKSUM,      -(MULTIBOOT2_MAGIC+MULTIBOOT2_HEADER_LENGTH+MULTIBOOT2_ARCHITECTURE)
-
-.section .multiboot2
 
 multiboot2_header_begin:
 
@@ -29,17 +30,56 @@ multiboot2_header_begin:
 
 multiboot2_header_end:
 
-.equ KERNEL_STACK_SIZE, 4096
+/* Constant */
+.equ PAGE_SIZE_HALF,  0x800
+.equ PAGE_SIZE,       0x1000
+.equ PAGE_OFFSET,     0xC0000000
 
+/* Bootstrap data */
 .section .bss
 
-kernel_stack:
-.fill KERNEL_STACK_SIZE, 1, 0
+.align PAGE_SIZE
+bootstrap_pd:    .fill PAGE_SIZE, 1, 0
+bootstrap_stack: .fill PAGE_SIZE, 1, 0
 
+/* Bootstrap code */
 .section .text
 entry:
+  /* We cannot use eax and ebx as they contain information from multiboot2 */
+
+  /* Initialize page directory that maps 0GB-2GB to both 0GB-2GB and 2GB-4GB.
+   * Total number of entries is 1024. 512 for each mappings range respecitvely. */
+  xor %ecx, %ecx
+
+1:
+  mov %ecx, %edx
+  shl $22, %edx
+  or $0b000010000011, %edx
+
+  mov %edx, (bootstrap_pd + 0             )(,%ecx, 4)
+  mov %edx, (bootstrap_pd + PAGE_SIZE_HALF)(,%ecx, 4)
+
+  inc %ecx
+  cmp $512, %ecx
+  jne 1b
+
+  /* Enable PSE(Page Size Extension) */
+  mov %cr4, %ecx
+  or $0x00000010, %ecx
+  mov %ecx, %cr4
+
+  /* Load the page directory */
+  mov $bootstrap_pd, %ecx
+  mov %ecx, %cr3
+
+  /* Enable paging */
+  mov %cr0, %ecx
+  or $0x80000001, %ecx
+  mov %ecx, %cr0
+
+  /* Load the stack */
   xor %ebp, %ebp
-  mov $(kernel_stack + KERNEL_STACK_SIZE), %esp
+  mov $(bootstrap_stack + PAGE_SIZE), %esp
 
   push %ebx
   push %eax

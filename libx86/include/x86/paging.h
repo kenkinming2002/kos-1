de97c216f2595e00ad7ba707d940c2281a1ae282
KenKwok <kenkinming2002@gmail.com>
2022-11-08 21:42:51 +0800

    Add page map shift, mask and size constant

--- libx86/include/x86/paging.h
+++ libx86/include/x86/paging.h
@@ -8,9 +8,24 @@
 #define PAGE_UNCACHED        ((uint64_t)1 << 4)
 #define PAGE_ACCESSED        ((uint64_t)1 << 5)
 #define PAGE_DIRTY           ((uint64_t)1 << 6)
-#define PAGE_SIZE            ((uint64_t)1 << 7)
+#define PAGE_SIZE_EXTENSION  ((uint64_t)1 << 7)
 #define PAGE_GLOBAL          ((uint64_t)1 << 8)
 #define PAGE_EXECUTE_DISABLE ((uint64_t)1 << 63)
+
+#define PML1_SHIFT 12
+#define PML2_SHIFT 21
+#define PML3_SHIFT 30
+#define PML4_SHIFT 39
+
+#define PML1_SIZE ((uint64_t)1 << PML1_SHIFT)
+#define PML2_SIZE ((uint64_t)1 << PML2_SHIFT)
+#define PML3_SIZE ((uint64_t)1 << PML3_SHIFT)
+#define PML4_SIZE ((uint64_t)1 << PML4_SHIFT)
+
+#define PML1_MASK ~(PML1_SIZE-1)
+#define PML2_MASK ~(PML2_SIZE-1)
+#define PML3_MASK ~(PML3_SIZE-1)
+#define PML4_MASK ~(PML4_SIZE-1)
 
 typedef uint64_t pme_t;
 typedef uint64_t pm_t[512];

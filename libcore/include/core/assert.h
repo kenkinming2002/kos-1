ed72f5be80ab20666a8c565619a4b9badf8f4bd3
KenKwok <kenkinming2002@gmail.com>
2022-11-13 23:01:40 +0800

    core: mark kassert as noreturn

--- libcore/include/core/assert.h
+++ libcore/include/core/assert.h
@@ -1,7 +1,7 @@
 #ifndef CORE_ASSERT_H
 #define CORE_ASSERT_H
 
-void kassert(const char *file, int line);
+__attribute__((noreturn)) void kassert(const char *file, int line);
 #define KASSERT(expr) do { if(!(expr)) kassert(__FILE__, __LINE__); } while(0)
 
 #endif // CORE_ASSERT_H

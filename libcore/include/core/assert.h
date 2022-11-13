23ee9309530197eb6898dcfb2e88052ca2440368
KenKwok <kenkinming2002@gmail.com>
2022-11-13 23:02:55 +0800

    core: add KASSERT_UNREACHABLE

--- libcore/include/core/assert.h
+++ libcore/include/core/assert.h
@@ -3,5 +3,6 @@
 
 __attribute__((noreturn)) void kassert(const char *file, int line);
 #define KASSERT(expr) do { if(!(expr)) kassert(__FILE__, __LINE__); } while(0)
+#define KASSERT_UNREACHABLE KASSERT(false && "Unreachable")
 
 #endif // CORE_ASSERT_H

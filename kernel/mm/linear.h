c51bbdb063da8771ba7ebe2a721481ade72cc572
KenKwok <kenkinming2002@gmail.com>
2022-11-24 23:40:19 +0800

    kernel: remove mm/

--- kernel/mm/linear.h
+++
@@ -1,9 +0,0 @@
-#ifndef MM_LINEAR_H
-#define MM_LINEAR_H
-
-#include "defs.h"
-
-static inline uintptr_t virt_to_phys(void *ptr)  { return (uintptr_t)ptr - LINEAR_MAP_BEGIN; }
-static inline void *phys_to_virt(uintptr_t addr) { return (void *)(addr + LINEAR_MAP_BEGIN); }
-
-#endif // MM_LINEAR_H

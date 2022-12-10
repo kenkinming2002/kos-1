d7cd5d9fbf2682fe02d0a5f0de3ffe0ae3c64bf7
KenKwok <kenkinming2002@gmail.com>
2022-12-11 00:30:45 +0800

    kernel: hal: add LOCAL_LOCK_INIT and SPIN_LOCK_INIT

--- kernel/hal/locks.h
+++ kernel/hal/locks.h
@@ -11,6 +11,8 @@ struct local_lock
 void local_lock(struct local_lock *lock);
 void local_unlock(struct local_lock *lock);
 
+#define LOCAL_LOCK_INIT (struct local_lock){0}
+
 struct spin_lock
 {
   uint64_t flags;
@@ -19,5 +21,7 @@ struct spin_lock
 
 void spin_lock(struct spin_lock *lock);
 void spin_unlock(struct spin_lock *lock);
+
+#define SPIN_LOCK_INIT (struct spin_lock){0, 0}
 
 #endif // LOCK_SPINLOCK_H

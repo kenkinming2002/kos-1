bee2dac2a8dc116d2df00623385a42f08873c605
KenKwok <kenkinming2002@gmail.com>
2023-03-15 14:40:28 +0800

    Implement strncpy(), strnlen() and strncmp()

--- libcore/include/core/string.h
+++ libcore/include/core/string.h
@@ -9,9 +9,13 @@ void *memset(void *dst, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 
 char *strcpy(char *restrict dst, const char *restrict src);
+char *strncpy(char *restrict dst, const char *restrict src, size_t n);
 
 size_t strlen(const char *s);
+size_t strnlen(const char *s, size_t n);
+
 int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 
 /* Copy string from src to a buffer pointed to by dst of length size.
  * Return the length of string copied. */
